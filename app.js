88cecd28c7617efa9605939b761fc073085afc5f
Adriana Perez <adpc1609@gmail.com>
2018-05-18 18:17:29 -0400

    boton nombre

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-window.onload = function() {
-    var name = prompt("¿cuál es tu nombre?");
+ function nameAccept() {
+        var resultado = document.getElementById("myText").value;
+        document.getElementById("name").innerHTML = "Bienvenida" + resultado;
+        ducument.getElementById("myText").style.display="none";
+        document.getElementById("nameButton").style.display="none";
+        
+    }
     document.getElementById("name").innerText = name;
-
     var wantToPlay = prompt("¿quieres jugar? s/n");
     if (wantToPlay.toLowerCase() == "s") {
         var answer1 = prompt("¿han egresado hombres en Laboratoria? s/n");
@@ -36,4 +40,3 @@ window.onload = function() {
     } else {
         document.getElementById("warningMessage").innerText = "Bueno Chao";
     }
-}
